fd7a1bddfd3d3ca9bd9b7e5ed7c68a5cd0a36fa7
Nooruz <noki9521@gmail.com>
2026-04-22 19:13:16 +0600

    Add base ValueObject abstraction

---
+++ KIT.GasStation.Domain/Abstractions/ValueObject.cs
@@ -0,0 +1,20 @@
+namespace KIT.GasStation.Domain.Abstractions;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not ValueObject other || other.GetType() != GetType())
+            return false;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(1, (current, obj) => HashCode.Combine(current, obj));
+    }
+}
